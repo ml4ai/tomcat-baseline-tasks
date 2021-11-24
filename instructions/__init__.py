aa627f73a4e65d5ab88ebd76df7adb96d7bebcd2
Eric Duong <eduong@email.arizona.edu>
2021-11-24 14:03:03 -0700

    prepare instruction classes

---
+++ instructions/__init__.py
@@ -0,0 +1,2 @@
+from .finger_tapping_task_instructions import FingerTappingTaskInstructions
+from .ping_pong_task_instructions import PingPongTaskInstructions
---
+++ instructions/ping_pong_task_instructions.py
@@ -0,0 +1,2 @@
+class PingPongTaskInstructions:
+    pass
