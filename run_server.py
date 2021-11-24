2e6a119832db5e333a8b159f308bcd115a5c9e2e
Eric Duong <eduong@email.arizona.edu>
2021-11-23 18:24:28 -0700

    ping pong multi-processes

--- run_server.py
+++ run_server.py
@@ -1,11 +1,17 @@
 import argparse
-
-import pygame
+from multiprocessing import Process
 
 from common import DEFAULT_SERVER_ADDR
 from network import Server
 from tasks.finger_tapping_task import ServerFingerTappingTask
 from tasks.ping_pong_task import ServerPingPongTask
+
+
+def run_ping_pong(to_client_connections: list, from_client_connections: dict):
+    server_ping_pong_task = ServerPingPongTask(to_client_connections, 
+                                               from_client_connections)
+    server_ping_pong_task.run()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run server of finger tapping task.')
@@ -26,6 +32,6 @@ if __name__ == "__main__":
 
     server.establish_connections()
 
-    server_ping_pong_task = ServerPingPongTask(list(server.to_client_connections.values()), 
-                                               server.from_client_connections)
-    server_ping_pong_task.run()
+    ping_pong_process = Process(target=run_ping_pong, args=(server.to_client_connections.values(), server.from_client_connections))
+    ping_pong_process.start()
+    ping_pong_process.join()
