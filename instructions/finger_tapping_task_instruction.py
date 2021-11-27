3db9bd54ae4827e6a8190d3e71ffb98d7522ab2f
Eric Duong <eduong@email.arizona.edu>
2021-11-26 18:27:16 -0700

    extra information

--- instructions/finger_tapping_task_instruction.py
+++ instructions/finger_tapping_task_instruction.py
@@ -4,10 +4,10 @@ from .utils import instruction, wait_for_experimenter
 
 
 def finger_tapping_task_instruction(to_server, screen, client_name):
-    image = pygame.image.load("instructions/images/more_pinkiepie.png")
+    image = pygame.image.load("instructions/images/TomCat_BaslineIntructions01-Introduction.png")
     instruction(image, screen)
 
-    image = pygame.image.load("instructions/images/pinkiepie.jpg")
+    image = pygame.image.load("instructions/images/TomCat_BaselineInstructions02-FingerTapingGame_Explanation.png")
     instruction(image, screen)
 
     wait_for_experimenter(to_server, screen, client_name)
--- tasks/finger_tapping_task/config_finger_tapping_task.py
+++ tasks/finger_tapping_task/config_finger_tapping_task.py
@@ -1,12 +1,12 @@
 # 0 for self tapping, 1 for collaborative tapping
 SESSION = [0, 1, 0, 1]
 
-SECONDS_PER_SESSION = [3.0, 3.0, 3.0, 3.0]
+SECONDS_PER_SESSION = [30.0, 30.0, 30.0, 30.0]
 
 # count down before start
-SECONDS_COUNT_DOWN = 3.0
+SECONDS_COUNT_DOWN = 60.0
 
 # game sprite
 BOX_WIDTH = 200
 
-STARTING_MESSAGE = "Please press the SPACEBAR on keyboard"
+STARTING_MESSAGE = "Practice session: Press SPACEBAR and observe the squares"
