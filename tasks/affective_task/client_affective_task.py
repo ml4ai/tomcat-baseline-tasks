7931fcb7bb9fbc9d450b186896e6706e0282fe87
Eric Duong <eduong@email.arizona.edu>
2022-01-17 14:00:45 -0700

    timer for response

--- tasks/affective_task/client_affective_task.py
+++ tasks/affective_task/client_affective_task.py
@@ -4,7 +4,7 @@ import pygame
 from common import receive, send
 
 from .utils import (REFRESH_RATE, Button, render_image_center,
-                    render_text_center)
+                    render_text_center, timer)
 
 
 class IndexButtonArousal(Enum):
@@ -41,8 +41,6 @@ class ClientAffectiveTask:
         button_valence_minus_1 = Button((200, 350), "-1", self._screen)
         button_valence_minus_2 = Button((400, 350), "-2", self._screen)
 
-        clock = pygame.time.Clock()
-
         print("[STATUS] Running affective task")
 
         while True:
@@ -55,20 +53,7 @@ class ClientAffectiveTask:
             state = data["state"]
             render_image_center(state["image_path"], self._screen, refresh=True)
             
-            start_ticks = pygame.time.get_ticks()
-            while True:
-                seconds_has_passed = (pygame.time.get_ticks() - start_ticks) / 1000.0
-                seconds_left_to_count = state["image_timer"] - seconds_has_passed
-                if seconds_left_to_count < 0.0:
-                    break
-                else:
-                    seconds_left_to_count = 0 if seconds_left_to_count < 0.0 else int(seconds_left_to_count)
-                    if state["collaboration"]:
-                        render_text_center("Team: " + str(seconds_left_to_count), (300, 50), self._screen, y_offset=-420)
-                    else:
-                        render_text_center("Individual: " + str(seconds_left_to_count), (300, 50), self._screen, y_offset=-420)
-
-                clock.tick(REFRESH_RATE)
+            timer(state["image_timer"], [], "Team: " if state["collaboration"] else "Individual: ", self._screen)
 
             render_image_center("./tasks/affective_task/images/buttons_images/Valence.jpg", 
                                 self._screen, 
@@ -90,25 +75,13 @@ class ClientAffectiveTask:
             button_valence_minus_1.render()
             button_valence_minus_2.render()
 
-            start_ticks = pygame.time.get_ticks()
-            while True:
+            def button_response():
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         if button_arousal_minus_2.object.collidepoint(pygame.mouse.get_pos()):
                             print("Something")
-            
-                seconds_has_passed = (pygame.time.get_ticks() - start_ticks) / 1000.0
-                seconds_left_to_count = state["rating_timer"] - seconds_has_passed
-                if seconds_left_to_count < 0.0:
-                    break
-                else:
-                    seconds_left_to_count = 0 if seconds_left_to_count < 0.0 else int(seconds_left_to_count)
-                    if state["collaboration"]:
-                        render_text_center("Team: " + str(seconds_left_to_count), (300, 50), self._screen, y_offset=-420)
-                    else:
-                        render_text_center("Individual: " + str(seconds_left_to_count), (300, 50), self._screen, y_offset=-420)
 
-                clock.tick(REFRESH_RATE)
+            timer(state["image_timer"], [button_response], "Team: " if state["collaboration"] else "Individual: ", self._screen)
 
             # TODO: submit valid responses
             response = {
--- tasks/affective_task/utils/__init__.py
+++ tasks/affective_task/utils/__init__.py
@@ -3,3 +3,4 @@ from .config import REFRESH_RATE
 from .get_image_paths import get_image_paths
 from .render_image_center import render_image_center
 from .render_text_center import render_text_center
+from .timer import timer
