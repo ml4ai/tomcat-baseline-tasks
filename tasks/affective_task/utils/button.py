ab1b9bfc6c80d16b30bf5e491c2090998817dc0c
Eric Duong <eduong@email.arizona.edu>
2022-02-09 14:00:51 -0700

    drawing frames

--- tasks/affective_task/utils/button.py
+++ tasks/affective_task/utils/button.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 
 import pygame
-from common import COLOR_DIM, COLOR_FOREGROUND
+from common import COLOR_BACKGROUND, COLOR_FOREGROUND
 from config import CLIENT_WINDOW_HEIGHT, CLIENT_WINDOW_WIDTH
 
 BOX_WIDTH = 100
@@ -11,26 +11,27 @@ BOX_HEIGHT = 200
 class Button:
     def __init__(self, offset: Tuple[int, int], screen):
         x_offset, y_offset = offset
-        x = CLIENT_WINDOW_WIDTH / 2 + x_offset
-        y = CLIENT_WINDOW_HEIGHT / 2 + y_offset
+        x = CLIENT_WINDOW_WIDTH / 2 + x_offset - BOX_WIDTH / 2
+        y = CLIENT_WINDOW_HEIGHT / 2 + y_offset - BOX_HEIGHT / 2
 
         self._position = (x, y)
         self._screen = screen
 
-        self.object = None
+        self.object = pygame.draw.rect(self._screen, pygame.Color(0, 0, 0, 0), pygame.Rect(x, y, BOX_WIDTH, BOX_HEIGHT))
         self._selected = False
 
     def render(self):
         x, y = self._position
-        x -= int(BOX_WIDTH / 2)
-        y -= int(BOX_HEIGHT / 2)
 
         if self._selected:
             color = COLOR_FOREGROUND
         else:
-            color = COLOR_DIM
+            color = (200, 200, 200)
 
-        self.object = pygame.draw.rect(self._screen, color, (x, y, BOX_WIDTH, BOX_HEIGHT))
+        pygame.draw.line(self._screen, color, (x, y), (x + BOX_WIDTH , y), 3)
+        pygame.draw.line(self._screen, color, (x, y - 2), (x, y + BOX_HEIGHT), 3)
+        pygame.draw.line(self._screen, color, (x, y + BOX_HEIGHT), (x + BOX_WIDTH , y + BOX_HEIGHT), 3)
+        pygame.draw.line(self._screen, color, (x + BOX_WIDTH , y+BOX_HEIGHT), [x + BOX_WIDTH , y], 3)
 
         pygame.display.flip()
 
--- tasks/affective_task/utils/rating_buttons.py
+++
@@ -1,6 +0,0 @@
-from typing import Tuple
-
-
-class RatingButtons:
-    def __init__(self, image_path: str, offset: Tuple[int, int], screen) -> None:
-        pass
