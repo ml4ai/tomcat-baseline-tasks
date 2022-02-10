68f9bfa6cb25dabf198dfeb1d5451dcdb79152c9
Eric Duong <eduong@email.arizona.edu>
2022-02-10 10:53:45 -0700

    revert configure changes

--- tasks/finger_tapping_task/config_finger_tapping_task.py
+++ tasks/finger_tapping_task/config_finger_tapping_task.py
@@ -1,10 +1,10 @@
 # 0 for self tapping, 1 for collaborative tapping
-SESSION = [0]
+SESSION = [0, 1, 0, 1]
 
-SECONDS_PER_SESSION = [1.0]
+SECONDS_PER_SESSION = [15.0, 15.0, 15.0, 15.0]
 
 # count down before start
-SECONDS_COUNT_DOWN = 1.0
+SECONDS_COUNT_DOWN = 10.0
 
 # game sprite
 SQUARE_WIDTH = 200
