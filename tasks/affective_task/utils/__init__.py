5014027a7c63c4f2b8ffe111858216b0e5e73816
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2022-02-17 13:56:58 -0700

    submit button (#34)

--- tasks/affective_task/utils/__init__.py
+++ tasks/affective_task/utils/__init__.py
@@ -2,4 +2,5 @@ from .button import Button
 from .get_image_paths import get_image_paths
 from .render_image_center import render_image_center
 from .render_text_center import render_text_center
+from .submit_button import submit_button
 from .timer import timer
